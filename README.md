3f7ff33646c6843a538383356e7ecf67de9a2762
Lee Waggoner <lwaggoner@roadrunner.com>
2024-02-29 09:38:12 -0800

    Updated gradle.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # valuewatch
-Value Watch is more than just a clock—it's a bold statement against pointless meetings and time-wasting activities. With its large visual display, Value Watch vividly converts your time into money in real-time.
+Value Watch is more than just a clock —- it's a bold statement against pointless meetings and time-wasting activities. With its large visual display, Value Watch vividly converts your time into money in real-time.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.wreckingballsoftware.valuewatch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.wreckingballsoftware.valuewatch"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,17 +30,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
         resources {
@@ -50,8 +51,7 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
